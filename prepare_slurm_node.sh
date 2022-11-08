1bd6c098a763d89fb8823c65d2c95b875e86785e
Martin Gruber <martin.gruber1@web.de>
2022-11-08 23:13:24 +0100

    Improve logging

--- prepare_slurm_node.sh
+++ prepare_slurm_node.sh
@@ -1,4 +1,7 @@
+echo "-- (Un)setting global variables"
 unset XDG_RUNTIME_DIR
 unset XDG_CONFIG_HOME
 export HOME=$PODMAN_HOME
+
+echo "-- Creating alias 'p'"
 alias p='podman --root=$LOCAL_PODMAN_ROOT'
