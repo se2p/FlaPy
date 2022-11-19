00f771483fff7980b6232a5bee1e515f95ecf474
Martin Gruber <martin.gruber1@web.de>
2022-11-19 22:24:19 +0100

    Add documentation

--- setup_docker_command.sh
+++ setup_docker_command.sh
@@ -4,6 +4,9 @@
 # 1. define variable FLAPY_DOCKER_IMAGE
 # 2. define function flapy_docker_command
 
+# This script can either be modified, or a custom script can be used instead by setting the
+# FLAPY_DOCKER_COMMAND_SETUP_SCRIPT variable
+
 echo "-- Define FlaPy docker image"
 export FLAPY_DOCKER_IMAGE="registry.hub.docker.com/gruberma/flapy"
 
