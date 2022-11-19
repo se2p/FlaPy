ce3f2de2c452d2457720aae7b0aec86326b9271e
Martin Gruber <martin.gruber1@web.de>
2022-11-19 11:40:28 +0100

    Rename cleaning scripts

---
+++ docker_rm_all_containers.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# -- SET UP ENVIRONMENT (define flapy_docker_command)
+echo "-- Prepare for docker command"
+source prepare_for_docker_command.sh || exit
+
+echo "-- Removing containers"
+flapy_docker_command rm -v --force --all
+
+echo "-- Echo podman info"
+./echo_podman_info.sh
