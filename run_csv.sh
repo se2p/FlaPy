3be8cced4580bc09171e771033b8efd88ac280ad
Martin Gruber <martin.gruber1@web.de>
2022-11-20 22:19:49 +0100

    Improve usage message

--- run_csv.sh
+++ run_csv.sh
@@ -11,12 +11,16 @@ if [[ "$#" -lt 5 ]]; then
 
     RUN_ON must be either 'locally' or 'cluster'
     CONSTRAINT is the \`sbatch --constraint\` in case RUN_ON == 'cluster'
-    INPUT_CSV is the flapy input csv file, which must have the following columns in the following order:
-        PROJECT_NAME, PROJECT_URL, PROJECT_HASH, PYPI_TAG, FUNCS_TO_TRACE, TESTS_TO_BE_RUN, NUM_RUNS
+    INPUT_CSV is the flapy input csv file,
+        which must have the following columns in the following order:
+        PROJECT_NAME, PROJECT_URL, PROJECT_HASH, PYPI_TAG, FUNCS_TO_TRACE, TESTS_TO_RUN, NUM_RUNS
     PLUS_RANDOM_RUNS must be 'true' or 'false'
-    FLAPY_ARGS can contain the following, but must always be provided, even as empty string. Must always be one string
-        --random-order-seed
-    OUT_DIR is the parent folder of the output results directory. If this option is not provided, the current directory is used
+    FLAPY_ARGS can contain the following, but must always be provided, even as empty string.
+        Must always be one string.
+        Available options:
+        --random-order-seed <seed>
+    OUT_DIR is the parent folder of the output results directory.
+        If this option is not provided, the current directory is used
 
 Example: $0 locally \"\" flapy_input_example.csv false \"\" example_results
 "
