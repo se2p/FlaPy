ca8eeacd5a9c213be4b491e3764cdcd31a5f961e
Martin Gruber <martin.gruber1@web.de>
2022-11-21 23:32:01 +0100

    Refactor preprocessing script redirect_over_csv.py

--- flapy/preprocess_repos/redirect_over_csv.py
+++ flapy/preprocess_repos/redirect_over_csv.py
@@ -17,32 +17,35 @@ def resolve_url(url: str) -> Tuple[str, int]:
     num_retries = 0
     num_max_retries = 6
     sleep_time = 11
-    response = session.get(url)
-    while response.status_code == 429 and num_retries < num_max_retries:
-        num_retries += 1
-        print(f"Sleeping {sleep_time}s due to 429 ({num_retries}/{num_max_retries})", file=sys.stderr)
-        sleep(sleep_time)
+    try:
         response = session.get(url)
-    if num_retries == num_max_retries:
-        print(f"ERROR: sleeping didn't help for {url}", file=sys.stderr)
-    return response.url, response.status_code
+        while response.status_code == 429 and num_retries < num_max_retries:
+            num_retries += 1
+            print(f"Sleeping {sleep_time}s due to 429 ({num_retries}/{num_max_retries})", file=sys.stderr)
+            sleep(sleep_time)
+            response = session.get(url)
+        if num_retries == num_max_retries:
+            print(f"ERROR: sleeping didn't help for {url}", file=sys.stderr)
+        return response.url, response.status_code
+    except Exception as e:
+        return "", f"ERROR: {type(e).__name__}: {e}."
 
 
 def main(args: List[str] = None):
     if args is None:
         args = sys.argv[1:]
 
-    if len(args) != 2:
-        print("USAGE: python redirect_over_csv.py REPOS_CSV OUTPUT_CSV", file=sys.stderr)
+    if len(args) != 3:
+        print("USAGE: python redirect_over_csv.py REPOS_CSV OUTPUT_CSV URL_COLUMN", file=sys.stderr)
         print(file=sys.stderr)
         print(
-            "REPOS_CSV should have two columns (Project_Name, Project_URL)",
+            "REPOS_CSV must have columns named URL_COLUMN",
             file=sys.stderr,
         )
         sys.exit()
 
     # -- PARSE ARGUMENTS
-    input_csv, output_csv = args
+    input_csv, output_csv, url_column = args
 
     # -- READ CSV
     df = pd.read_csv(input_csv)
@@ -51,12 +54,12 @@ def main(args: List[str] = None):
     # -- Resolve URLs
     #  do not try to parallelize this, it will just give you a 429 (too many requests)
     print("Resolving URLs", end="", file=sys.stderr)
-    df["response"] = [resolve_url(url) for url in tqdm(df["Project_URL"])]
+    df["response"] = [resolve_url(url) for url in tqdm(df[url_column])]
 
     # -- Prettify Dataframe
-    df["Project_URL_redirected"] = df["response"].apply(lambda x: x[0])
-    df["Project_URL_status"] = df["response"].apply(lambda x: x[1])
-    df["same_redirect"] = (df["Project_URL"] == df["Project_URL_redirected"])
+    df[f"{url_column}_redirected"] = df["response"].apply(lambda x: x[0])
+    df[f"{url_column}_status"] = df["response"].apply(lambda x: x[1])
+    df["same_redirect"] = (df[url_column] == df[f"{url_column}_redirected"])
     df = df.drop(columns="response")
 
     session.close()
