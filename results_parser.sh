448c1607a4e53c02aac6afa3eb3bd96ab06a88ca
Martin Gruber <martin.gruber1@web.de>
2022-12-26 20:17:23 +0100

    Add -it flag when running container for results_parser

--- results_parser.sh
+++ results_parser.sh
@@ -9,7 +9,7 @@ export DEBUG=0
 debug_echo "-- Prepare for docker command"
 source "$SCRIPT_DIR/prepare_for_docker_command.sh" || exit
 
-flapy_docker_command run --rm --entrypoint=results_parser \
+flapy_docker_command run --rm -it --entrypoint=results_parser \
     -v "$(pwd)":/mounted_cwd --workdir /mounted_cwd \
     $FLAPY_DOCKER_IMAGE "$@"
 
