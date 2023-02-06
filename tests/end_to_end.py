1591b241714da5aa27b93c52dceb9f696db0c34d
Martin Gruber <martin.gruber1@web.de>
2023-02-06 22:06:02 +0100

    Make end-to-end test manual

--- tests/end_to_end.py
+++ tests/end_to_end.py
@@ -45,7 +45,7 @@ def test_end_to_end():
     logging.info(f"ERROR: {err}")
 
     # CHECK OUTPUT
-    assert len(list(out_dir.rglob("*"))) == 15
+    assert len(list(out_dir.rglob("*"))) == 9
 
     # PARSE
     logging.info("PARSING")
