64ba7ad920ead65b7ee5a699b725c98fc1cfd0dd
Martin Gruber <martin.gruber1@web.de>
2025-01-26 16:15:32 +0100

    Improve squeue_statistic_array.sh

--- squeue_statistic_array.sh
+++ squeue_statistic_array.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Running:     $(squeue --me --array --noheader -t R  | wc -l)"
-echo "Pending:     $(squeue --me --array --noheader -t PD | wc -l)"
-echo "Configuring: $(squeue --me --array --noheader -t CF | wc -l)"
-echo "Total:       $(squeue --me --array --noheader       | wc -l)"
+RUNNING=$(squeue --me --array --noheader -t R  | wc -l)
+PENDING=$(squeue --me --array --noheader -t PD | wc -l)
+CONFING=$(squeue --me --array --noheader -t CF | wc -l)
+TOTAL=$(  squeue --me --array --noheader       | wc -l)
+
+echo "Running:     $RUNNING"
+echo "Pending:     $PENDING"
+echo "Configuring: $CONFING"
+echo "Total:       $TOTAL"
+
+echo "$(date -Iseconds),$RUNNING,$PENDING,$CONFING,$TOTAL" >> squeue_statistic_array_progression.csv
